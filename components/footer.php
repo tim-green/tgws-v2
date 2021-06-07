bdc570489aacfc279d24955d927b8fa7b208a661
Tim <github@timgreen.ws>
2021-06-07 15:21:19 +1000

    added includes for footer partials

---
+++ components/footer.php
@@ -0,0 +1,9 @@
+	<div class="content large-paragraph">
+		<?php 
+			/*include social and code networks*/ 
+			include_once('partials/footer/foot-contacts.php'); 
+			
+			/*include social and code networks*/ 
+			include_once('partials/footer/the-networks.php');
+		?>
+	</div>
