558682d3d81f85a970f4099e6a0343e0de0855d1
Tim <github@timgreen.ws>
2021-06-07 15:21:02 +1000

    added social networks to partials folder

---
+++ partials/footer/the-networks.php
@@ -0,0 +1,16 @@
+<nav id="social-networks">
+					<h2 class="visually-hidden">Social Networks</h2>
+					<p>Otherwise you can stalk me on these social networks:</p>
+					<ul>
+						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Dribbble"><span><svg class="icon" width="24" height="24"><use xlink:href="/portfolio/assets/images/icons.svg#dribbble"></use></svg><span class="visually-hidden">GitHub</span></span></a>
+						</li>
+						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Medium"><span><svg class="icon" width="24" height="24"><use xlink:href="/portfolio/assets/images/icons.svg#medium"></use></svg><span class="visually-hidden">Blog</span></span></a>
+						</li>
+						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Twitter"><span><svg class="icon" width="24" height="24"><use xlink:href="/portfolio/assets/images/icons.svg#twitter"></use></svg><span class="visually-hidden">SWC IG</span></span></a>
+						</li>
+						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Instagram"><span><svg class="icon" width="24" height="24"><use xlink:href="/portfolio/assets/images/icons.svg#instagram"></use></svg><span class="visually-hidden">Instagram</span></span></a>
+						</li>
+						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Facebook"><span><svg class="icon" width="24" height="24"><use xlink:href="/portfolio/assets/images/icons.svg#facebook"></use></svg><span class="visually-hidden">Facebook</span></span></a>
+						</li>
+					</ul>
+				</nav>
