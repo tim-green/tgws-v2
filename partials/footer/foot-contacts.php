de1d96f19a2259b3f1dc5667a935f704d860ece5
Tim <github@timgreen.ws>
2021-06-07 15:20:50 +1000

    added footer contacts to partials folder

---
+++ partials/footer/foot-contacts.php
@@ -0,0 +1,10 @@
+<nav id="contacts">
+				<h2 class="visually-hidden">Contacts</h2>
+					<p>If you want to contact me for new projects and collaborations
+						<br>or just talking about the two chief world systems:</p>
+					<ul>
+						<li><a class="button" href="#"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#email"></use></svg> Write me <strong>E-Mail</strong></span></a></li>
+						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#telegram"></use></svg> Let&rsquo;s have a chat<strong>FaceTime</strong></span></a></li>
+						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a></li>
+					</ul>
+				</nav>
