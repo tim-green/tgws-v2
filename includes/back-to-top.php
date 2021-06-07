f0df301597a6cb013555f2fe135b37ffdeb86f97
Tim <github@timgreen.ws>
2021-06-07 17:20:01 +1000

    back to top scroll

---
+++ includes/back-to-top.php
@@ -0,0 +1,6 @@
+<p class="back-to-top">
+    <a class="button button-rounded button-icon" href="#cover">
+        <span class="visually-hidden">Back to Top</span>
+        <i class="fal fa-arrow-up"></i>
+    </a>
+</p>
