db06f7ed0e15fdb50520de070b36d93be3ee9fda
Tim <github@timgreen.ws>
2021-06-07 15:24:20 +1000

    removed async from app.min.js

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
     <footer id="footer" class="full-viewport vertically-centered">
     </footer>
 <!-- js -->
-<script src="assets/build/app.min.js" async></script>
+<script src="assets/build/app.min.js"></script>
 <script src="https://kit.fontawesome.com/c20cab8581.js" crossorigin="anonymous"></script>
 <!-- back to top -->
 <?php include_once('includes/back-to-top.php'); ?>
