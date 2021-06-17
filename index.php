611ec17c04c30aaaee105e918fc9d1d8bc44cea5
Tim <github@timgreen.ws>
2021-06-17 14:06:02 +1000

    updated meta

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
   <link rel="shortcut icon" href="/assets/favicon/favicon.ico">
   <meta name="apple-mobile-web-app-title" content="Pietro">
   <meta name="application-name" content="Pietro">
+  <meta name="apple-mobile-web-app-title" content="Tim Green">
+  <meta name="application-name" content="Tim Green">
   <meta name="msapplication-TileColor" content="#11998e">
   <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">
 
