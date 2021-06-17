874b26b68d05b60c284475d6ac83eb4f19f18498
Tim <github@timgreen.ws>
2021-06-17 15:04:35 +1000

    added menu links - first alliteration

---
+++ partials/header/menu.php
@@ -0,0 +1,63 @@
+<nav id="menu">
+	<h2 class="visually-hidden">Skip to content</h2>
+		<ul>
+
+            <li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Bio + Skills</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+
+			<li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Selected Works</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+			
+
+            <li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Experience</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+
+
+            <li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Notables</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+            
+            <li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Blog</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+
+            <li>
+                <a class="button button-rounded button-shadow smooth-scroll" href="#selected-works">
+                    <span class="label">Contact</span>
+                    <span class="icon">
+                    <i class="fal fa-arrow-down neon-gradient"></i>
+                    </span>
+                </a>
+			</li>
+
+            
+        </ul>
+</nav>
