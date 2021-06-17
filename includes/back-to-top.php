9acaf5e8cd7e7d4856ea805a6720eebae1b0af93
Tim <github@timgreen.ws>
2021-06-17 16:05:32 +1000

    added neon-gradient class for arrow

--- includes/back-to-top.php
+++ includes/back-to-top.php
@@ -1,6 +1,6 @@
 <p class="back-to-top">
     <a class="button button-rounded button-icon" href="#cover">
         <span class="visually-hidden">Back to Top</span>
-        <i class="fal fa-arrow-up"></i>
+        <i class="fal fa-arrow-up neon-gradient"></i>
     </a>
 </p>
