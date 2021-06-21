91ce91eda7cc356b062784c69591ca51057f1ded
Tim <github@timgreen.ws>
2021-06-21 13:14:19 +1000

    added structure tags for header

--- components/header.php
+++ components/header.php
@@ -1,12 +1,21 @@
 				<p class="footnote ">
 <header id="cover" class="full-viewport">
+		<div class="wrapper">
+			<div>
+				<div>
 					<div id="cover-animation">
+						<div>
 							<!-- title -->
 							<h1 class="display-heading">Hello, I&rsquo;m Tim<span class="visually-hidden"> Green</span>. <br><span class="neon-gradient">I code experiences <br>to connect brands <br>with people.</span></h1>
+						</div>
+
 						<!-- main site menu -->
 						<?php include_once('partials/header/menu.php'); ?>
 					</div>
+				</div>
 				<p class="footnote visually-hidden">
 				<?php include_once('partials/legal-notice.php'); ?>
 				</p>
+			</div>
+		</div>
 	</header>
