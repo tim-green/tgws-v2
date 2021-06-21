2377d9f2a46495a73386462294afebbc6afb4541
Tim <github@timgreen.ws>
2021-06-21 13:14:33 +1000

    added div for logo - svg

--- components/header.php
+++ components/header.php
@@ -1,10 +1,12 @@
-				<p class="footnote ">
 <header id="cover" class="full-viewport">
 		<div class="wrapper">
 			<div>
 				<div>
 					<div id="cover-animation">
 						<div>
+							<div class="logo">
+								<!-- svg -->
+							</div>
 							<!-- title -->
 							<h1 class="display-heading">Hello, I&rsquo;m Tim<span class="visually-hidden"> Green</span>. <br><span class="neon-gradient">I code experiences <br>to connect brands <br>with people.</span></h1>
 						</div>
