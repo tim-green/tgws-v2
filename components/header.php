33ae0c7194271b01b02c2bac4c9457451ceda853
Tim <github@timgreen.ws>
2021-06-21 13:13:20 +1000

    added display heading

--- components/header.php
+++ components/header.php
@@ -1,5 +1,7 @@
 				<p class="footnote ">
 <header id="cover" class="full-viewport">
+							<!-- title -->
+							<h1 class="display-heading">Hello, I&rsquo;m Tim<span class="visually-hidden"> Green</span>. <br><span class="neon-gradient">I code experiences <br>to connect brands <br>with people.</span></h1>
 						<!-- main site menu -->
 						<?php include_once('partials/header/menu.php'); ?>
 				<p class="footnote visually-hidden">
