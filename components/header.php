7174c417b90ccb589bbf97ab7b645c3cf6b98e49
Tim <github@timgreen.ws>
2021-06-21 13:12:49 +1000

    added cover tag for header

--- components/header.php
+++ components/header.php
@@ -1,4 +1,6 @@
 				<p class="footnote ">
+<header id="cover" class="full-viewport">
 				<p class="footnote visually-hidden">
 				<?php include_once('partials/legal-notice.php'); ?>
 				</p>
+	</header>
