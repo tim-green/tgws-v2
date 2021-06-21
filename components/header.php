70172354136811b9f1e66dabff9fb667b31ca6f8
Tim <github@timgreen.ws>
2021-06-21 13:13:33 +1000

    cover animation

--- components/header.php
+++ components/header.php
@@ -1,9 +1,11 @@
 				<p class="footnote ">
 <header id="cover" class="full-viewport">
+					<div id="cover-animation">
 							<!-- title -->
 							<h1 class="display-heading">Hello, I&rsquo;m Tim<span class="visually-hidden"> Green</span>. <br><span class="neon-gradient">I code experiences <br>to connect brands <br>with people.</span></h1>
 						<!-- main site menu -->
 						<?php include_once('partials/header/menu.php'); ?>
+					</div>
 				<p class="footnote visually-hidden">
 				<?php include_once('partials/legal-notice.php'); ?>
 				</p>
