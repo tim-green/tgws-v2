7afa62e28b122b400c287fb730d80cf2cd74f281
Tim <github@timgreen.ws>
2021-06-21 15:17:36 +1000

    change id for top function

--- includes/back-to-top.php
+++ includes/back-to-top.php
@@ -1,5 +1,5 @@
 <p class="back-to-top">
-    <a class="button button-rounded button-icon" href="#cover">
+    <a class="button button-rounded button-icon" href="#tgws">
         <span class="visually-hidden">Back to Top</span>
         <i class="fal fa-arrow-up neon-gradient"></i>
     </a>
