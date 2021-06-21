269fee467acc21d8e5c79087e21e57a0afaf3a41
Tim <github@timgreen.ws>
2021-06-21 13:13:06 +1000

    added included file for menu items

--- components/header.php
+++ components/header.php
@@ -1,5 +1,7 @@
 				<p class="footnote ">
 <header id="cover" class="full-viewport">
+						<!-- main site menu -->
+						<?php include_once('partials/header/menu.php'); ?>
 				<p class="footnote visually-hidden">
 				<?php include_once('partials/legal-notice.php'); ?>
 				</p>
