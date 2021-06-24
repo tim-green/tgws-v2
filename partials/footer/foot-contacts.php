a54d1282c5bae444dab073194202b3604b13f9e9
Tim <github@timgreen.ws>
2021-06-24 13:31:30 +1000

    updated footer contact text

--- partials/footer/foot-contacts.php
+++ partials/footer/foot-contacts.php
@@ -1,7 +1,7 @@
 <nav id="contacts">
 				<h2 class="visually-hidden">Contacts</h2>
 					<p>If you want to contact me for new projects and collaborations
-						<br>or just talking about the two chief world systems:</p>
+						<br>or just talking about something else:</p>
 					<ul>
 						<li><a class="button" href="#"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#email"></use></svg> Write me <strong>E-Mail</strong></span></a></li>
 						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#telegram"></use></svg> Let&rsquo;s have a chat<strong>FaceTime</strong></span></a></li>
