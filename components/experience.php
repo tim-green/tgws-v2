965e2fa330c1bb5942f1a5026d2e8a256a9e3aa1
Tim <github@timgreen.ws>
2021-06-28 10:33:56 +1000

    added experience content to file

--- components/experience.php
+++ components/experience.php
@@ -2,4 +2,41 @@
 	<p class="display-heading neon-gradient" data-text="Experience">
 		<span>Experience</span>
 	</p>
+		
+	<div class="content large-paragraph">
+    <nav id="exp">
+				<h2 class="visually-hidden hidden">Experience</h2>
+					<ul>
+						<li>
+							<a class="button" href="#">
+								<span> 
+								Freelance</br>
+								2019 - Current  
+								<strong>Developer</strong>
+								</span>
+							</a>
+						</li>
+
+						<li>
+							<a class="button" href="#">
+								<span> 
+								2B Advertising and Design</br>
+								2011-2018  
+								<strong>Web Developer</strong>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a class="button" href="#">
+								<span> 
+								Roof Rack City Canberra</br>
+								2008-2010  
+								<strong>Website Developer</strong>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+	</div>
+	
 </div>
