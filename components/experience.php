d31aec7cebdf54994070a50b789fbca45bb07a56
Tim <github@timgreen.ws>
2021-07-05 13:29:24 +1000

    cleaned up html

--- components/experience.php
+++ components/experience.php
@@ -1,31 +1,31 @@
 <div class="wrapper" id="experience">
-	<p class="display-heading neon-gradient" data-text="Experience">
+	<h3 class="large-heading neon-gradient" data-text="Experience">
 		<span>Experience</span>
-	</p>
-		
+	</h3>
 	<div class="content" id="exp">
-				<h2 class="visually-hidden hidden">Experience</h2>
-
-				<div class="grid max-3 expandable">
-				<div>
-					<h4 class="small-heading">Freelance Developer</h4>
-					<div class="content">
-						<p>2019 - Current<br/>Developer</p>
-					</div>
-				</div>
-				<div>
-				<h4 class="small-heading">2B Advertising and Design</h4>
-					<div class="content">
-						<p>2011 - 2018  <br/>Web Developer</p>
-					</div>
-				</div>
-				<div>
-				<h4 class="small-heading">Roof Rack City Canberra</h4>
-					<div class="content">
-						<p>2008-2010 <br/>Web Developer</p>
-					</div>
+		<h2 class="visually-hidden hidden">Experience</h2>
+		<div class="grid max-3 expandable">
+			<div>
+				<h4 class="small-heading">Freelance Developer</h4>
+				<div class="content">
+					<p><span class="yet-exp">2019 - Current<br/>Developer</span>
+					</p>
 				</div>
 			</div>
+			<div>
+				<h4 class="small-heading">2B Advertising and Design</h4>
+				<div class="content">
+					<p><span class="yet-exp">2011 - 2018<br/>Web Developer</span>
+					</p>
+				</div>
+			</div>
+			<div>
+				<h4 class="small-heading">Roof Rack City Canberra</h4>
+				<div class="content">
+					<p><span class="yet-exp">2008 - 2010<br/>Web Developer</span>
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
-	
 </div>
