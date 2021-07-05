4aa555744720a92ea88947651fef27c572d526ea
Tim <github@timgreen.ws>
2021-07-05 13:29:43 +1000

    added new file for notables

---
+++ components/notables.php
@@ -0,0 +1,22 @@
+<h3 class="large-heading neon-gradient" data-text="Notable">
+		<span>Notable</span>
+	</h3>
+
+    <div class="content" id="exp">
+				<h2 class="visually-hidden hidden">Experience</h2>
+
+				<div class="grid max-3 expandable">
+				<div>
+					<h4 class="small-heading">Font Awesome Pro</h4>
+					<div class="content">
+						<p><span class="yet-exp">2016<br/>1 out of 35,550 Kickstarter Backer</span></p>
+					</div>
+				</div>
+				<div>
+				<h4 class="small-heading">Hotjar</h4>
+					<div class="content">
+						<p><span class="yet-exp">2015<br/>Founding Members (Beta Tester)</span></p>
+					</div>
+				</div>
+			</div>
+	</div>
