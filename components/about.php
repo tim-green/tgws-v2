0afbfc9934208cb4b559201522b288dea7cc0bf0
Tim <github@timgreen.ws>
2021-07-12 11:40:02 +1000

    added bio/skill heading

--- components/about.php
+++ components/about.php
@@ -1,6 +1,7 @@
 <div class="wrapper">
 			<div id="tg-biography">
 				<div>
+					<h3 class="large-heading neon-gradient skills-heading" data-text="Bio and Skills"><span>Bio + Skills</span></h3>
 				</div>
 			</div>
 		</div>
