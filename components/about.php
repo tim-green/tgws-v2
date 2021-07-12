2ec24a0f2da946ceb517cf974a5fac4361a034dc
Tim <github@timgreen.ws>
2021-07-12 11:40:19 +1000

    added about content to page

--- components/about.php
+++ components/about.php
@@ -2,6 +2,17 @@
 			<div id="tg-biography">
 				<div>
 					<h3 class="large-heading neon-gradient skills-heading" data-text="Bio and Skills"><span>Bio + Skills</span></h3>
+					<div class="content">
+					<p>I'm a web developer with a passion for strong, simple websites. I have 13 years of experience working in both an advertising agency and freelance capacity, mainly focusing on building websites and web applications which are simple to use and look great. </p>
+
+					<p> I've worked with some great agencies, government organisations, individuals and start-ups to their reach both business and website goals. My approach is realistic and creative, supported by skills in web development such as using <span class="skill">HTML</span>, <span class="skill">CSS/Sass</span>, <span class="skill">PHP</span>, <span class="skill">JavaScript/jQuery</span> and along with using frameworks like <span class="skill">Bootstrap</span>.</p>
+
+					<p> I have experience using <span class="skill">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, and I can also create Accessible PDF and Word Documents. And even can create and manage email campaigns using <span class="skill">MailChimp</span>.</p>
+					
+					<p>I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>. I use <span class="skill">GruntJS</span> to automatically perform frequent tasks that I use on a daily when building websites and while using <span class="skill">GitHub</span> and <span class="skill">Fork Git Client</span> to help with versioning control my code.</p>
+
+					<p>Every week I use a social media management tool called <span class="skill">HootSuite</span> where I create and schedule social media posts to Facebook and Instagram about the upcoming posts on my web design blog called SomeWhat Creative. <span id="bio-skills"></span></p>
+					</div>
 				</div>
 			</div>
 		</div>
