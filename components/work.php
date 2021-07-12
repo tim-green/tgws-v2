db64edaaa4a09d12da88f9a8e97074669dd26b2d
Tim <github@timgreen.ws>
2021-07-12 13:48:40 +1000

    added includes for work partials to see overall flow of content

--- components/work.php
+++ components/work.php
@@ -2,5 +2,12 @@
 
 <span id="projects"></span>
 <?php
-include 'work/work-template.php';
+include 'partials/work/work-template.php';
+include 'partials/work/work-afma.php';
+include 'partials/work/work-rsl.php';
+include 'partials/work/work-automotiv.php';
+include 'partials/work/work-booth.php';
+include 'partials/work/work-nighowl.php';
+include 'partials/work/work-swc.php';
+
 ?>
