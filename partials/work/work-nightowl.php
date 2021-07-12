d4dccdd1952e9bb0ac46a6cb9a9fcedaa7593d3f
Tim <github@timgreen.ws>
2021-07-12 13:48:14 +1000

    created all selected work files, added default work framework content

---
+++ partials/work/work-nightowl.php
@@ -0,0 +1,40 @@
+<article id="id-of-project" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">UI Design</p>
+				<h3 class="large-heading">Night Owl</h3>
+				<div class="content">
+					<p>Dummy text is coming here now: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. .</p>
+				</div>
+				<dl class="details">
+					<dt>Client</dt>
+					<dd>Client Name</dd>
+					<dt>Agency</dt>
+					<dd>Freelance</dd>
+					<dt>Year</dt>
+					<dd>2222</dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="http://cdn.timgreen.xyz/tgv2/gamez.jpg" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/matchbox/multicolored wallpaper.jpg" width="800" height="600" alt="Menu">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/matchbox/video-bg.jpg" width="800" height="600" alt="Menu">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/tgv2/gamez.jpg" width="800" height="600" alt="Menu">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
