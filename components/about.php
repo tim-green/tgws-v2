0f9d803c046a6999de3d2d3ec4c328a34cd38379
Tim <github@timgreen.ws>
2021-07-12 11:40:51 +1000

    added currently building content to page

--- components/about.php
+++ components/about.php
@@ -23,6 +23,20 @@
                         To expand my knowledge I'm currently learning ReactJS, you can check out some of my React projects that I've coded up here I'm also learning Adobe XD so that I can improve my UI/UX design and prototyping. 
 						</p>
 					</div>
+
+
+					<h1 class="large-heading neon-gradient second-level-heading">What I'm currently building</span>
+                    </h1>
+					<div class="content">
+						<p>
+						I'm currently working on developing some of my own <span class="skill">WordPress plugins</span>, I've developed one plugin called Greeny Cookie Consent, and this plugin is simple and developer-friendly plugin that let visitors know that the site is using cookies. You can view the <a href="//github.com/tim-green/greeny-cookie-consent" rel="nofollow"><span class="skill">GitHub Repo</span></a> where's the plugin is hosted.
+						</p>
+
+                        <p>
+                        And also I'm in the progress in developing a clean responsive WooCommerce theme called <span class="skill">Automotiv</span> which you can <a href="//themes.timgreen.ws/automotiv/" rel="nofollow"><span class="skill">visit the site here</span></a>, or view the <a href="//github.com/tim-green/automotiv-wp-theme" rel="nofollow"><span class="skill">GitHub Repo</span></a>, also I've created my first <a href="//www.npmjs.com/package/tim-green-resume" rel="nofollow"><span class="skill">NPM package</span></a> which is based on my resume.
+                        </p>
+					</div>
+					
 				</div>
 			</div>
 		</div>
