0c4df606ef2addfed3c8d3814695a377ad0ef275
Tim <github@timgreen.ws>
2021-07-19 11:10:08 +1000

    added ID for scroll to top

--- includes/back-to-top.php
+++ includes/back-to-top.php
@@ -1,6 +1,7 @@
 <p class="back-to-top">
-    <a class="button button-rounded button-icon" href="#tgws">
+    <a class="button button-rounded button-icon" id="scrollToTopBtn" >
         <span class="visually-hidden">Back to Top</span>
         <i class="fal fa-arrow-up neon-gradient"></i>
     </a>
 </p>
+
