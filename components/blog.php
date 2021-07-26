741cd48156bdee7af8731193d05a108318fdb67d
Tim <github@timgreen.ws>
2021-07-26 12:43:43 +1000

    updated method from xml to wp-json for post

---
+++ components/blog.php
@@ -0,0 +1,42 @@
+<header id="blog" class="full-viewport">
+	<div class="wrapper">
+        <div>
+        <h2 class="large-heading second-level-heading" data-text="Latest post from SomeWhat Creative">
+            <span>Latest post from SomeWhat Creative</span> <span class="visually-hidden">Latest post from SomeWhat Creative</span>
+        </h2>
+
+        <?php 
+        $url="https://somewhatcreative.net/wp-json/wp/v2/posts?per_page=1&_embed"; 
+        $ch= curl_init(); 
+    
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_URL,$url); 
+        
+        $result=curl_exec($ch); 
+        $posts= json_decode($result, true); 
+        foreach ($posts as $post) 
+        { 
+        ?>
+
+        <div class="content">
+            <h4 class="medium-heading second-level-heading">
+            <a href="<?php echo $post['link']; ?>" class="external-linkBlog" rel="follow" target="_blank">
+		    
+            <?php 
+            echo $post['title']['rendered']; ?>
+		    
+            <small>Posted On: <?php echo date('F j, Y', strtotime($post['date'])); ?></small>
+        </a>
+        </h4>
+        <?php 
+            echo $post[ 'excerpt'][ 'rendered']; 
+        ?>
+            </div>
+        <?php } ?>
+        </div>
+	</div>
+</header>
+
+
+		
