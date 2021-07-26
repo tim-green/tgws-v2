78d4e81414df70e0f4c661b76b81ccca3ad1b693
Tim <github@timgreen.ws>
2021-07-26 14:07:25 +1000

    added links to mail,github & linkedin

--- partials/footer/foot-contacts.php
+++ partials/footer/foot-contacts.php
@@ -7,4 +7,9 @@
 						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#telegram"></use></svg> Let&rsquo;s have a chat<strong>FaceTime</strong></span></a></li>
 						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a></li>
 					</ul>
-				</nav>
+				</nav>		<li><a class="button" href="mailto:tim@timgreen.ws"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/assets/icons/envelope.svg"></use></svg> Write me an <strong>E-Mail</strong></span></a>
+		</li>
+		<li><a class="button" href="https://www.github.com/tim-green" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Take a look at my <strong>GitHub</strong></span></a>
+		</li>
+		<li><a class="button" href="https://www.linkedin.com/in/thetimgreen/" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a>
+		</li>
