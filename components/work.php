b8a5b27842e04a69d444846058c21c906a289ff2
Tim <github@timgreen.ws>
2021-08-02 12:22:48 +1000

    added atsb file

--- components/work.php
+++ components/work.php
@@ -3,6 +3,7 @@
 <span id="projects"></span>
 <?php
 include 'partials/work/work-template.php';
+include 'partials/work/work-atsb.php';
 include 'partials/work/work-rsl.php';
 include 'partials/work/work-automotiv.php';
 include 'partials/work/work-booth.php';
