980dcb5d2db774152f65058ca6d60d365d1f1e83
Tim <github@timgreen.ws>
2021-08-02 12:22:40 +1000

    removed afma file

--- components/work.php
+++ components/work.php
@@ -3,7 +3,6 @@
 <span id="projects"></span>
 <?php
 include 'partials/work/work-template.php';
-include 'partials/work/work-afma.php';
 include 'partials/work/work-rsl.php';
 include 'partials/work/work-automotiv.php';
 include 'partials/work/work-booth.php';
