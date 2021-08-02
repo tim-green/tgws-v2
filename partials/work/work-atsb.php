3937cd059c20c464894c7a74800b02853bc2c4a3
Tim <github@timgreen.ws>
2021-08-02 12:24:26 +1000

    added new work slider for ATSB

---
+++ partials/work/work-atsb.php
@@ -0,0 +1,40 @@
+<article id="afma" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">HTML Report</p>
+				<h3 class="large-heading">ATSB Annual Report 2016-17</h3>
+				<div class="content">
+					<p>Dummy text is coming here now: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. .</p>
+				</div>
+				<dl class="details">
+					<dt>Client</dt>
+					<dd>ATSB</dd>
+					<dt>Agency</dt>
+					<dd>2B Advertising & Design</dd>
+					<dt>Year</dt>
+					<dd>2017</dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="http://cdn.timgreen.xyz/tgv2/gamez.jpg" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/matchbox/multicolored wallpaper.jpg" width="800" height="600" alt="Menu">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/matchbox/video-bg.jpg" width="800" height="600" alt="Menu">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="http://cdn.timgreen.xyz/tgv2/gamez.jpg" width="800" height="600" alt="Menu">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
