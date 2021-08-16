876a2969ac29807d610681c47f192defa8efbd7c
Tim <github@timgreen.ws>
2021-08-16 16:14:54 +1000

    updated contact info

--- partials/footer/foot-contacts.php
+++ partials/footer/foot-contacts.php
@@ -1,15 +1,13 @@
 <nav id="contacts">
-				<h2 class="visually-hidden">Contacts</h2>
-					<p>If you want to contact me for new projects and collaborations
-						<br>or just talking about something else:</p>
-					<ul>
-						<li><a class="button" href="#"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#email"></use></svg> Write me <strong>E-Mail</strong></span></a></li>
-						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#telegram"></use></svg> Let&rsquo;s have a chat<strong>FaceTime</strong></span></a></li>
-						<li><a class="button" href="#" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a></li>
-					</ul>
-				</nav>		<li><a class="button" href="mailto:tim@timgreen.ws"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/assets/icons/envelope.svg"></use></svg> Write me an <strong>E-Mail</strong></span></a>
+	<h2 class="visually-hidden">Contacts</h2>
+	<p>If you want to contact me for new projects and collaborations
+		<br>or just talking about something else:</p>
+	<ul>
+		<li><a class="button" href="mailto:tim@timgreen.ws"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/assets/icons/envelope.svg"></use></svg> Write me an <strong>E-Mail</strong></span></a>
 		</li>
 		<li><a class="button" href="https://www.github.com/tim-green" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Take a look at my <strong>GitHub</strong></span></a>
 		</li>
 		<li><a class="button" href="https://www.linkedin.com/in/thetimgreen/" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a>
 		</li>
+	</ul>
+</nav>
