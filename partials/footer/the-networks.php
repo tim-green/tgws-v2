b780a98b4802cd078df4c35e521072d66ffeb1a7
Tim <github@timgreen.ws>
2021-08-16 16:14:30 +1000

    added fb,ig,gh & PDF icons + details to social network bar

--- partials/footer/the-networks.php
+++ partials/footer/the-networks.php
@@ -1,29 +1,26 @@
 <nav id="social-networks">
-					<h2 class="visually-hidden">Social Networks</h2>
-					<p>Otherwise you can stalk me on these social networks:</p>
-					<ul>
-						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="GitHub"><span>
-						<i class="fab fa-github neon-gradient"></i>	
-						<span class="visually-hidden">GitHub</span></span></a>
-						</li>
-						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="SomeWhat Creative"><span>
-						<i class="fab fa-wordpress-simple neon-gradient"></i>	
-						<span class="visually-hidden">Blog</span></span></a>
-						</li>
-						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="SomeWhat Creative IG"><span>
-						<i class="fab fa-instagram neon-gradient"></i>	
-						<span class="visually-hidden">SWC IG</span></span></a>
-						</li>
-						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Instagram"><span>
+	<h2 class="visually-hidden">Social Networks</h2>
+	<p>Otherwise you can stalk me on these few social networks or <br>view my resume in PDF form or an alt resume as a NPM Package:</p>
+	<ul>
+	<li><a class="button button-icon" rel="external noopener" href="//instagram.com/somewhatcreative" target="_blank" title="Instagram"><span>
 						<i class="fab fa-instagram neon-gradient"></i>	
 						<span class="visually-hidden">Instagram</span></span></a>
-						</li>
-						<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Facebook"><span>
+		</li>
+		<!-- <li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="Facebook"><span>
 						<i class="fab fa-facebook-square neon-gradient"></i>	
-						<span class="visually-hidden">Facebook</span></span></a>
-						</li>
-					</ul>
-				</nav>		<li><a class="button button-icon" rel="external noopener" href="https://www.timgreen.ws/tim-green-resume" target="_blank" title="Tim Green's Resume in PDF Form"><span>
+						<span class="visually-hidden">Facebook</span></span></a>		
+		</li> 
+
+		<li><a class="button button-icon" rel="external noopener" href="#" target="_blank" title="GitHub"><span>
+						<i class="fab fa-github neon-gradient"></i>	
+						<span class="visually-hidden">GitHub</span></span></a>
+		</li>
+		-->
+		<li><a class="button button-icon" rel="external noopener" href="//somewhatcreative.net" target="_blank" title="SomeWhat Creative Blog"><span>
+						<i class="fab fa-wordpress-simple neon-gradient"></i>	
+						<span class="visually-hidden">Blog</span></span></a>
+		</li>
+		<li><a class="button button-icon" rel="external noopener" href="timgreen.ws/tim-green-resume" target="_blank" title="Tim Green's Resume in PDF Form"><span>
 						<i class="fal fa-file-pdf neon-gradient"></i>	
 						<span class="visually-hidden">PDF</span></span></a>
 		</li>
@@ -31,3 +28,5 @@
 						<i class="fab fa-npm neon-gradient"></i>	
 						<span class="visually-hidden">NPM Package</span></span></a>
 		</li>
+	</ul>
+</nav>
