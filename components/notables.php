a5e7c27b8c1e73ca7048bc64e5499cc2f496ae32
Tim <github@timgreen.ws>
2021-08-17 13:38:09 +1000

    added new class of not-box

--- components/notables.php
+++ components/notables.php
@@ -4,14 +4,14 @@
 <div class="content" id="notables">
 	<h2 class="visually-hidden hidden">Notable</h2>
 	<div class="grid max-3">
-		<div>
+		<div class="not-box">
 			<h4 class="small-heading">Font Awesome Pro</h4>
 			<div class="content">
 				<p><span class="yet-exp">2016<br/>1 out of 35,550 Kickstarter Backer</span>
 				</p>
 			</div>
 		</div>
-		<div>
+		<div class="not-box">
 			<h4 class="small-heading">Hotjar</h4>
 			<div class="content">
 				<p><span class="yet-exp">2015<br/>Founding Members (Beta Tester)</span>
