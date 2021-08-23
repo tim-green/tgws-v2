ada89b0bf6efea9bd3b1637ef06231a15b80d697
Tim <github@timgreen.ws>
2021-08-23 16:39:05 +1000

    added fontawesome icons

--- partials/footer/foot-contacts.php
+++ partials/footer/foot-contacts.php
@@ -3,11 +3,12 @@
 	<p>If you want to contact me for new projects and collaborations
 		<br>or just talking about something else:</p>
 	<ul>
-		<li><a class="button" href="mailto:tim@timgreen.ws"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/assets/icons/envelope.svg"></use></svg> Write me an <strong>E-Mail</strong></span></a>
+		<li><a class="button" href="mailto:tim@timgreen.ws"><span><i class="far fa-envelope neon-gradient icon icon-fotcon"></i>
+		Write me an <strong>E-Mail</strong></span></a>
 		</li>
-		<li><a class="button" href="https://www.github.com/tim-green" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Take a look at my <strong>GitHub</strong></span></a>
+		<li><a class="button" href="https://www.github.com/tim-green" rel="external noopener" target="_blank"><span><i class="fab fa-github neon-gradient icon icon-fotcon"></i> Take a look at my <strong>GitHub</strong></span></a>
 		</li>
-		<li><a class="button" href="https://www.linkedin.com/in/thetimgreen/" rel="external noopener" target="_blank"><span><svg class="icon" width="48" height="48"><use fill="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#linkedin"></use></svg> Let&rsquo;s connect <strong>LinkedIn</strong></span></a>
+		<li><a class="button" href="https://www.linkedin.com/in/thetimgreen/" rel="external noopener" target="_blank"><span><i class="fab fa-linkedin neon-gradient icon icon-fotcon"></i> Let&rsquo;s connect <strong>LinkedIn</strong></span></a>
 		</li>
 	</ul>
 </nav>
