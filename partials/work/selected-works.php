83035e4a4dc4d1b5edbf04214f8a29edbed88213
Tim <github@timgreen.ws>
2021-08-23 12:56:52 +1000

    new file for selected works heading

---
+++ partials/work/selected-works.php
@@ -0,0 +1,12 @@
+<article class="white-background">
+<span id="projects"></span>
+	<div class="wrapper no-padding">
+    <div>
+        <h3 class="large-heading black-text" data-text="Selected Works">
+    <span>Selected Works</span> 
+    <span class="visually-hidden">Selected works</span>
+</h3>
+			
+</div>
+	</div>
+</article>
