10413de38797a2d40b47988d301c58aeff3084ab
Tim <github@timgreen.ws>
2021-09-07 15:07:21 +1000

    added IP info react project

---
+++ partials/work/work-all-react.php
@@ -0,0 +1,35 @@
+<!-- react 1 -->
+<article id="react-1" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">IP Info</h3>
+				<div class="content">
+					<p>This is a React app that interacts with <a href="https://ipinfo.io/" rel="nofollow" target="_blank">IPinfo API</a> to get the user IP number and information about the IP number such as the country of origin, region and city.
+                    </p>
+                    <span>Screenshot been taken with VPN on</span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="//ipinfo.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="//github.com/tim-green/ip-info-react" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-ipinfo.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-ipinfo.webp" width="800" height="600" alt="React IPinfo">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
