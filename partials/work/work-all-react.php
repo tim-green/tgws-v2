18d4f11a63bb9ba735b822e86147e99df9d65c7e
Tim <github@timgreen.ws>
2021-09-07 15:08:38 +1000

    added film base react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -79,3 +79,43 @@
 </article>
 
 
+<!-- react 3 -->
+<article id="react-3" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">Film Base</h3>
+				<div class="content">
+					<p>A React that fetches the data from TMDB (The Movie Database) API. It also displays the movie rating, budget, runtime and official site if it is avialable. 
+                    </p>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://movies.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/movdab" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-filmbase.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-filmbase.webp" width="800" height="600" alt="">
+					</div>
+                    <div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-filmbase-2.webp" width="800" height="600" alt="">
+					</div>
+                    <div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-filmbase-3.webp" width="800" height="600" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
