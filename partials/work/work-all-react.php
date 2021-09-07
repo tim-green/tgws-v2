ce92638dccb8feb6183425c00f6a93f4d91029b9
Tim <github@timgreen.ws>
2021-09-07 15:08:14 +1000

    added password generator react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -33,3 +33,49 @@
 	</div>
 </article>
 
+
+<!-- react 2 -->
+<article id="react-2" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">The Password Generator</h3>
+				<div class="content">
+					<p>This is a secure password and passphrase generator built with React
+                    </p>
+                    <p>It also features some helpful hints that help you keep good password hygeiene. This app doesn't keep any information</p>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="//pw-gen.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="//github.com/tim-green/password-generator" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-pwgen-1.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-pwgen-1.webp" width="800" height="600" alt="React IPinfo">
+					</div>
+					<div class="gallery-item dark">
+                    <img class="gallery-lazy" data-src="../assets/images/react-slides/react-pwgen-2.webp" width="800" height="600" alt="React IPinfo">
+					</div>
+                    <div class="gallery-item dark">
+                    <img class="gallery-lazy" data-src="../assets/images/react-slides/react-pwgen-3.webp" width="800" height="600" alt="React IPinfo">
+					</div>
+                    <div class="gallery-item dark">
+                    <img class="gallery-lazy" data-src="../assets/images/react-slides/react-pwgen-4.webp" width="800" height="600" alt="React IPinfo">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
+
