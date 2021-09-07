7d8ed0976b62dabc08ae8108381f1663a8418aef
Tim <github@timgreen.ws>
2021-09-07 15:08:50 +1000

    added RT ipsum react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -119,3 +119,46 @@
 	</div>
 </article>
 
+
+
+<!-- react 4 -->
+<article id="react-4" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">RT Ipsum</h3>
+				<div class="content">
+					<p>I’m a fan of a company called Rooster Teeth, there are an online video production company that provides myself with some great entertainment
+					</p>
+					<p>
+Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built a Lorem Ipsum generator for the times I need some filler text.
+                    </p>
+                    <span></span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="https://rt-ipsum.herokuapp.com" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/rt-ipsum" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-rt.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-rt.webp" width="800" height="600" alt="">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-rt-1.webp" width="800" height="600" alt="">
+					</div>
+                    
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
