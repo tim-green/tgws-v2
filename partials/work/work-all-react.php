1f5eaeb9b55a22b27dfd5954fe80d6a314dc5d3f
Tim <github@timgreen.ws>
2021-09-07 15:09:10 +1000

    added groundctrl react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -162,3 +162,48 @@ Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built 
 		</div>
 	</div>
 </article>
+
+
+<!-- react 5 -->
+<article id="react-5" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">GroundCTRL React UI</h3>
+				<div class="content">
+					<p>As I'm reading, learning up and understand UI and UX principles along the side of learning React I decided to build a React based User Interface Dashboard
+                    </p>
+					<p>The content in this Dashboard is using the filler text from my RT Lorem Ipsum React app..</p>
+                    <span></span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://groundctrl-ui.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/groundctrl-ui" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-groundctrl-1.webp" width="800" height="600" alt="">
+					</div>
+                    <div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-groundctrl-2.webp" width="800" height="600" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
+
