552984a86339ad599d63b9516bf3c29e86d32de1
Tim <github@timgreen.ws>
2021-09-07 15:09:23 +1000

    added emoji search react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -207,3 +207,41 @@ Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built 
 </article>
 
 
+<!-- react 6 -->
+<article id="react-6" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">Emoji Search</h3>
+				<div class="content">
+					<p>This is a super simple emoji search tool made with React. It features 20 different emojis when launching the app.
+                    </p>
+					<p>
+					If you wanting to view more emojis you simply can search for Santa (for example), and instantly you'll see all the various Santa based emoticons.
+					</p>
+                    <span></span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://emoji.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/emoji-search-react" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-emoji.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-emoji.webp" width="800" height="600" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
