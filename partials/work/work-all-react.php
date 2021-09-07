c54b15f47d4ec495726cdf1e5768b3933fa74104
Tim <github@timgreen.ws>
2021-09-07 15:10:13 +1000

    added todoist react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -376,3 +376,50 @@ Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built 
 	</div>
 </article>
 
+
+<!-- react 10 -->
+<article id="react-10" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">Todoist Clone</h3>
+				<div class="content">
+					<p>A Clone of the popular Task Manager App Todoist. </p>
+					
+					<p>Using Firestone to store all the to do's and while been built with React it also uses React hooks, useState, useEffect. 
+                    </p>
+                    <span></span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://todo-demo.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/todoist-clone-react" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-todo.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-todo.webp" width="800" height="600" alt="">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-todo-1.webp" width="800" height="600" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
+
+
+
+
+
+
