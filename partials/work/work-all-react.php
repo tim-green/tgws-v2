ee5fbca80e3ec3b7b7ea6513f025a176522f516d
Tim <github@timgreen.ws>
2021-09-07 15:10:04 +1000

    added splash react project

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -335,3 +335,44 @@ Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built 
 	</div>
 </article>
 
+
+<!-- react 9 -->
+<article id="react-9" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">Splash</h3>
+				<div class="content">
+					<p>This is a clone of the stock photography site called <a href="https://unsplash.com/" rel="nofollow">Unsplash</a>.</p> 
+					
+					<p>It features a simple layout to display all the images. The app uses the <a href="https://unsplash.com/developers" rel="nofollow">Unsplash API</a> to grab all the images. 
+                    </p>
+                    <span></span>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://unsplash.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/splash-clone" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-unsplash.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-unsplash.webp" width="800" height="600" alt="">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-unsplash-1.webp" width="800" height="600" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
