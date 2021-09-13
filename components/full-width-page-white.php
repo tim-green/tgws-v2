e392f0fd3b01c7ed112392877d8623992e9768ea
Tim <github@timgreen.ws>
2021-09-13 15:58:47 +1000

    template for full width page - white

---
+++ components/full-width-page-white.php
@@ -0,0 +1,51 @@
+<section class="black-wrap full-viewport">
+<div class="wrapper">
+			<div id="">
+				<div>
+					<h2 class="visually-hidden">Profile &amp; CV</h2>
+					
+					<p class="visually-hidden">Tim Green</p>
+					<h1 class="large-heading neon-gradient">What I'm currently learning</span>
+                    </h1>
+					<!-- large-paragraph -->
+					<div class="content">
+						<p>
+                        To expand my knowledge I'm currently learning ReactJS, you can check out some of my React projects that I've coded up here I'm also learning Adobe XD so that I can improve my UI/UX design and prototyping. 
+						</p>
+					</div>
+
+
+                    <h1 class="large-heading neon-gradient second-level-heading">What I'm currently building</span>
+                    </h1>
+					<div class="content">
+						<p>
+						I'm currently working on developing some of my own <span class="skill">WordPress plugins</span>, I've developed one plugin called Greeny Cookie Consent, and this plugin is simple and developer-friendly plugin that let visitors know that the site is using cookies. You can view the <a href="//github.com/tim-green/greeny-cookie-consent" rel="nofollow"><span class="skill">GitHub Repo</span></a> where's the plugin is hosted.
+						</p>
+
+                        <p>
+                        And also I'm in the progress in developing a clean responsive WooCommerce theme called <span class="skill">Automotiv</span> which you can <a href="//themes.timgreen.ws/automotiv/" rel="nofollow"><span class="skill">visit the site here</span></a>, or view the <a href="//github.com/tim-green/automotiv-wp-theme" rel="nofollow"><span class="skill">GitHub Repo</span></a>, also I've created my first <a href="//www.npmjs.com/package/tim-green-resume" rel="nofollow"><span class="skill">NPM package</span></a> which is based on my resume.
+                        </p>
+					</div>
+
+					<h1 class="large-heading neon-gradient second-level-heading">Section Name</span>
+                    </h1>
+                    <h1 class="display-heading neon-gradient second-level-heading">Section Name</span>
+                    </h1>
+					<div class="content">
+						<p>
+						I'm a web developer with a passion for strong, simple websites. I have 12 years of experience working in both an advertising agency and freelance capacity, mainly focusing on building websites/web applications which are simple to use and look great. 
+						</p>
+					</div>
+					
+				</div>
+			</div>
+			<div id="modal-profile" class="modal">
+				<div id="modal-content" class="modal-content">
+					<div class="wrapper">
+						<button id="modal-profile-close" class="button button-rounded"><span class="icon close"><svg height="16" viewBox="0 0 16 16"><use stroke="url(#neon-gradient)" xlink:href="/portfolio/assets/images/icons.svg#plus"></use></svg></span><span class="visually-hidden">Close</span></button>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+</section>
