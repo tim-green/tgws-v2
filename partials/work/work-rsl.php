92d40e58b0a0b202188b00fe102ec031b669dbe5
Tim <github@timgreen.ws>
2021-09-13 15:56:38 +1000

    cleaned up html

--- partials/work/work-rsl.php
+++ partials/work/work-rsl.php
@@ -5,8 +5,10 @@
 				<p class="overline">Website</p>
 				<h3 class="large-heading">RSL National Office</h3>
 				<div class="content">
-					<p>In 2017 Returned and Services League of Australia National needed a re-design of their WordPress website.</p><p>I built this WordPress theme. Primary goal was to build a theme that is simplistic and easy to consume their content.</p> <p> For ease of managing content I used the Fusion Page Builder plugin which allowed RSL to manage their own content updates. </p> 
-</p>
+					<p>In 2017 Returned and Services League of Australia National needed a re-design of their WordPress website.</p>
+					<p>I built this WordPress theme. Primary goal was to build a theme that is simplistic and easy to consume their content.</p> 
+					
+					<p> For ease of managing content I used the Fusion Page Builder plugin which allowed RSL to manage their own content updates. </p> 
 				</div>
 				<dl class="details">
 					<dt>Client</dt>
