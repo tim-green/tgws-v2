70d66b25f0ab21d7bcbd9c86a6f2d9fa8fa1648d
Tim <github@timgreen.ws>
2021-09-13 20:44:16 +1000

    bold swc

--- components/about.php
+++ components/about.php
@@ -10,16 +10,16 @@
 					<!-- <h2 class="large-heading neon-gradient">A Web Developer living
 						<br>in Canberra.</span>
 					</h2> -->
-					<div class="content">
+					<div class="content" >
 					<p>I'm a web developer with a passion for strong, simple websites. I have 13 years of experience working in both an advertising agency and a freelance capacity, mainly focusing on building websites and web applications that are simple to use and look great.</p>
 
 					<p>I've worked with some great agencies, government organisations, individuals and start-ups to reach both business and website goals. My approach is realistic and creative, supported by skills in web development such as using <span class="skill">HTML</span>, <span class="skill">CSS/Sass</span>, <span class="skill">PHP</span>, <span class="skill">JavaScript/jQuery</span> and along with using frameworks like <span class="skill">Bootstrap</span>.</p>
 
-					<p> I have experience using <span class="skill">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, and I can also create Accessible PDF and Word Documents. And even can create and manage email campaigns using <span class="skill">MailChimp</span>.</p>
+					<p> I have experience using <span class="skill"  id="bio-skills">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, and I can also create Accessible PDF and Word Documents. And even can create and manage email campaigns using <span class="skill">MailChimp</span>.</p>
 					
 					<p>I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>. I use <span class="skill">GruntJS</span> to automatically perform frequent that I use on a daily basis when building websites and while using <span class="skill">GitHub</span> and <span class="skill">Fork Git Client</span> to help with versioning control my code.</p>
 
-					<p>Every week I use a social media management tool called <span class="skill">HootSuite</span> where I create and schedule social media posts to Facebook and Instagram about the upcoming posts on my web design blog called SomeWhat Creative. <span id="bio-skills"></span></p>
+					<p>Every week I use a social media management tool called <span class="skill">HootSuite</span> where I create and schedule social media posts to Facebook and Instagram about the upcoming posts on my web design blog called <a href="//somewhatcreative.net" rel="follow" target="_blank"><span class="skill">SomeWhat Creative</span></a>.</p>
 					</div>
 
 
