2d2128af267b604d51b6252f0d2fd2049b0c1cf0
Tim <github@timgreen.ws>
2021-09-13 17:06:50 +1000

    added favicon config file

---
+++ config/favicon.php
@@ -0,0 +1,8 @@
+<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="config/site.webmanifest">
+<link rel="mask-icon" href="assets/images/favicon/safari-pinned-tab.svg" color="#141414">
+<meta name="msapplication-TileColor" content="#141414">
+<meta name="msapplication-TileImage" content="assets/images/favicon/mstile-144x144.png">
+<meta name="theme-color" content="#141414">
