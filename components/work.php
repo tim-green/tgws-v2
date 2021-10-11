e1d370b71da3a73c83088b9461908b6a457f5aa3
Tim <github@timgreen.ws>
2021-10-11 13:20:17 +1100

    changed positions for some work

--- components/work.php
+++ components/work.php
@@ -1,10 +1,10 @@
 <!-- insert all work invidiual projects are located in /work -->
 <?php
     include 'partials/work/selected-works.php';
-    include 'partials/work/work-atsb.php';
-    include 'partials/work/work-rsl.php';
     include 'partials/work/work-automotiv.php';
-    include 'partials/work/work-booth.php';
+    include 'partials/work/work-atsb.php';
     include 'partials/work/work-nightowl.php';
+    include 'partials/work/work-rsl.php';
+    include 'partials/work/work-booth.php';
     include 'partials/work/work-swc.php';
 ?>
