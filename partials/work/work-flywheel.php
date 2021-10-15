e8dc3f478200448e07b6b0323fa41a136366627b
Tim <github@timgreen.ws>
2021-10-15 14:18:04 +1100

    added flywheel work to site

---
+++ partials/work/work-flywheel.php
@@ -0,0 +1,46 @@
+<?php /* flywheel project */ ?>
+<article id="flywheel" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">Drupal Theme</p>
+				<h3 class="large-heading">Flywheel</h3>
+				<div class="content">
+					<p>As someone who primarly develops WordPress based websites, I want to jump into developing and expand my knowledge of Drupal by making this basic theme for Drupal. Flywheel uses Bootstrap for the CSS framework, Flexslider for the home page slider and I use the Block layout within the Drupal administration dashboard to position content.
+					</p>
+
+				</div>
+				<dl class="details">
+					<dt>Client</dt>
+					<dd>Personal Concept</dd>
+					<dt>Year</dt>
+					<dd>2021</dd>
+					<dt>Visit</dt>
+					<dd><a href="//drupal.timgreen.ws" target="_blank">View Website</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="//github.com/tim-green/flywheel-drupal" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="assets/images/slides/preloaders/preloader-flywheel.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="assets/images/slides/flywheel-slide-1.webp" width="800" height="600" alt="Drupal theme called flywheel, home page featuring a Slider">
+					</div>
+					<div class="gallery-item  dark">
+						<img class="gallery-lazy" data-src="assets/images/slides/flywheel-slide-2.webp
+" width="800" height="600" alt="general page layout of the drupal theme">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/slides/flywheel-slide-3.webp
+" width="800" height="600" alt="responsive web view of flywheel">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
