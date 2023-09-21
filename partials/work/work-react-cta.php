b35ab793d5fd7c8032db09d78c79b960f12f7419
Tim <github@timgreen.ws>
2023-09-21 17:10:39 +1000

    added a CTA project for React page

---
+++ partials/work/work-react-cta.php
@@ -0,0 +1,48 @@
+<?php /* React CTA project */ ?>
+
+<!-- react 13 -->
+<article id="react-13" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading" id="rps-heading">Rock <br/>Paper<br/> Scissors</h3>
+				<div class="content">
+					<p>This is a React app of the classic and well loved game – Rock Paper Scissors. </p>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://rps.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/rps-react" target="_blank">View Repo</a></dd>
+					<dt>See More</dt>
+					<dd><a href="/react" target="_self">React Apps Here</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="assets/images/react-slides/react-rps-1.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-1.webp" width="800" height="600" alt="rock page sissors">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-2.webp" width="800" height="600" alt="mobile view of the todo list">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-3.webp" width="800" height="600" alt="mobile view of the todo list">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-4.webp" width="800" height="600" alt="mobile view of the todo list">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-5.webp" width="800" height="600" alt="mobile view of the todo list">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
