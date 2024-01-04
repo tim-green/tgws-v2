1e8eb3ddf1b0030ba98d202ea5b2a1a340ca28a5
Tim <github@timgreen.ws>
2024-01-04 13:28:28 +1100

    changed order of selected works

--- components/work.php
+++ components/work.php
@@ -2,10 +2,10 @@
 <?php
     include 'partials/work/selected-works.php';
     include 'partials/work/work-automotiv.php';
-    include 'partials/work/work-atsb.php';
     include 'partials/work/work-swc.php';
-    include 'partials/work/work-booth.php';
     include 'partials/work/work-nightowl.php';
+    include 'partials/work/work-atsb.php';
+    include 'partials/work/work-booth.php';
     include 'partials/work/work-flywheel.php';
     include 'partials/work/work-rsl.php';
     include 'partials/work/work-react-cta.php';
