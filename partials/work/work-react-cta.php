03cbe05ab4a9960ab4acdc964ca4cb939cfb8470
Tim <github@timgreen.ws>
2024-01-04 13:22:55 +1100

    changed React CTA

--- partials/work/work-react-cta.php
+++ partials/work/work-react-cta.php
@@ -1,20 +1,20 @@
 <?php /* React CTA project */ ?>
-
-<!-- react 13 -->
-<article id="react-13" class="project">
+<article id="react-5" class="project">
 	<div class="wrapper">
 		<header class="project-overview expandable">
 			<div>
 				<p class="overline">React</p>
-				<h3 class="large-heading" id="rps-heading">Rock <br/>Paper<br/> Scissors</h3>
+				<h3 class="large-heading">GroundCTRL React UI</h3>
 				<div class="content">
-					<p>This is a React app of the classic and well loved game – Rock Paper Scissors. </p>
+					<p>While delving into the realms of UI and UX principles alongside my exploration of React, I opted to construct a User Interface Dashboard based on React. 
+                    </p>
+                    <span></span>
 				</div>
 				<dl class="details">
 					<dt>Visit</dt>
-					<dd><a href="http://rps.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dd><a href="http://groundctrl-ui.timgreen.xyz/" target="_blank">View Project</a></dd>
 					<dt>Git Repo</dt>
-					<dd><a href="https://github.com/tim-green/rps-react" target="_blank">View Repo</a></dd>
+					<dd><a href="https://github.com/tim-green/groundctrl-ui" target="_blank">View Repo</a></dd>
 					<dt>See More</dt>
 					<dd><a href="/react" target="_self">React Apps Here</a></dd>
 				</dl>
@@ -23,23 +23,17 @@
 		<div class="project-content">
 			<div class="gallery">
 				<div class="gallery-placeholder">
-					<img class="lazy-inline" data-src="assets/images/react-slides/react-rps-1.webp" width="800" height="600" alt="Loading Image...">
+					<img class="lazy-inline" data-src="assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="Loading Image...">
 				</div>
 				<div class="gallery-wrapper">
 					<div class="gallery-item main dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-1.webp" width="800" height="600" alt="rock page sissors">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="ground control admin dashboard">
 					</div>
 					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-2.webp" width="800" height="600" alt="mobile view of the todo list">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl-1.webp" width="800" height="600" alt="Demo of graph within ground control">
 					</div>
-					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-3.webp" width="800" height="600" alt="mobile view of the todo list">
-					</div>
-					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-4.webp" width="800" height="600" alt="mobile view of the todo list">
-					</div>
-					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-rps-5.webp" width="800" height="600" alt="mobile view of the todo list">
+                    <div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl-2.webp" width="800" height="600" alt="Font size level headings">
 					</div>
 				</div>
 			</div>
