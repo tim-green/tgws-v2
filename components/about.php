2a74c88cac63418973c222bb629d4b4ddef0d67b
Tim <github@timgreen.ws>
2024-10-14 08:56:22 +1100

    updated skills

--- components/about.php
+++ components/about.php
@@ -11,13 +11,13 @@
 				<div class="content" >
 					<p>I'm a web developer with a passion for strong, simple websites. I have 16 years of experience working in both an advertising agency and a freelance capacity, mainly focusing on building websites and web applications that are simple to use and look great.</p>
 
-					<p>I've worked with some great agencies, government organisations, individuals and start-ups to reach both business and website goals. My approach is realistic and creative, supported by skills in web development such as using <span class="skill">HTML</span>, <span class="skill">CSS/Sass</span>, <span class="skill">PHP</span>, <span class="skill">JavaScript/jQuery</span> and along with using frameworks like <span class="skill">Bootstrap</span>. </p>
+					<p>I've worked with some great agencies, government organisations, individuals and start-ups to reach both business and website goals. My approach is realistic and creative, supported by skills in web development such as using <span class="skill">HTML</span>, <span class="skill">CSS/Sass</span>, <span class="skill">PHP</span>, <span class="skill">JavaScript</span>, <span class="skill">jQuery</span> and along with using frameworks like <span class="skill">Bootstrap</span>, <span class="skill">Tailwind CSS</span>. </p>
 
 					<p> I have experience using <span class="skill">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, and I can also create Accessible PDF and Word Documents. And even can create and manage email campaigns using <span class="skill">MailChimp</span>.</p>
 					<span id="about"></span>
 					<p>I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>. I use <span class="skill">GruntJS</span>, <span class="skill">ParcelJS</span> or <span class="skill">Webpack</span> to automatically perform task that I use on a daily basis when building websites and while using <span class="skill">GitHub</span> and <span class="skill">Fork Git Client</span> to help with versioning control my code.</p>
 
-					<p>Each week I use a social media management tool called Social Champ where I create and schedule social media posts to Facebook and Instagram about the upcoming posts on my web design blog called <a href="//somewhatcreative.net" rel="follow" target="_blank"><span class="bold-text neon-gradient-underline">SomeWhat Creative</span></a>. Additionally, I have experience with other social media management platforms such as HootSuite, Buffer, and AgoraPulse.</p>
+					<p>Each week I use a social media management tool called Social Champ where I create and schedule social media posts to Facebook and Instagram about the upcoming posts on my web design blog called <a href="//somewhatcreative.net" rel="follow" target="_blank"><span class="bold-text neon-gradient-underline">SomeWhat Creative</span></a>. Additionally, I have experience with other social media management platforms such as <span class="skill">HootSuite</span>, <span class="skill">Buffer</span>, and <span class="skill">AgoraPulse</span>.</p>
 				</div>
 
 
