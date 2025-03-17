84d322138c1ed098d285d5ddcfe95a21929dc009
Tim <github@timgreen.ws>
2025-03-17 13:44:03 +1100

    added Otter as the CTA

--- partials/work/work-react-cta.php
+++ partials/work/work-react-cta.php
@@ -1,20 +1,19 @@
 <?php /* React CTA project */ ?>
-<article id="react-5" class="project">
+<!-- react 3 -->
+<article id="react-3" class="project">
 	<div class="wrapper">
 		<header class="project-overview expandable">
 			<div>
 				<p class="overline">React</p>
-				<h3 class="large-heading">GroundCTRL React UI</h3>
+				<h3 class="large-heading" id="ott-heading">Otter</h3>
 				<div class="content">
-					<p>While delving into the realms of UI and UX principles alongside my exploration of React, I opted to construct a User Interface Dashboard based on React. 
-                    </p>
-                    <span></span>
+					<p>Otter is a movie information platform built with React, Redux, and Vite, using the TMDB API for dynamic movie data. The app has a responsive, easy-to-navigate UI that allows users to explore movie details, ratings, and cast.</p><p>Modern design elements like card-based previews and category filters make browsing seamless and efficient. This project highlights a user-focused interface and practical use of popular front-end technologies. </p>
 				</div>
 				<dl class="details">
 					<dt>Visit</dt>
-					<dd><a href="http://groundctrl-ui.timgreen.xyz/" target="_blank">View Project</a></dd>
+					<dd><a href="http://otter.timgreen.xyz/" target="_blank">View Project</a></dd>
 					<dt>Git Repo</dt>
-					<dd><a href="https://github.com/tim-green/groundctrl-ui" target="_blank">View Repo</a></dd>
+					<dd><a href="https://github.com/tim-green/tmdb-react-app" target="_blank">View Repo</a></dd>
 					<dt>See More</dt>
 					<dd><a href="/react" target="_self">React Apps Here</a></dd>
 				</dl>
@@ -23,17 +22,20 @@
 		<div class="project-content">
 			<div class="gallery">
 				<div class="gallery-placeholder">
-					<img class="lazy-inline" data-src="assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="Loading Image...">
+					<img class="lazy-inline" data-src="assets/images/react-slides/react-otter-slide-1.webp" width="800" height="600" alt="Loading Otter Images...">
 				</div>
 				<div class="gallery-wrapper">
 					<div class="gallery-item main dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl.webp" width="800" height="600" alt="ground control admin dashboard">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-otter-slide-1.webp" width="800" height="600" alt="Otter landing page">
 					</div>
 					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl-1.webp" width="800" height="600" alt="Demo of graph within ground control">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-otter-slide-2.webp" width="800" height="600" alt="Otter show details">
 					</div>
-                    <div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="assets/images/react-slides/react-groundctrl-2.webp" width="800" height="600" alt="Font size level headings">
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-otter-slide-3.webp" width="800" height="600" alt="show trailer player and footer design">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="assets/images/react-slides/react-otter-slide-4.webp" width="800" height="600" alt="404 Page design and responsive design elements">
 					</div>
 				</div>
 			</div>
