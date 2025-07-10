397523ffa63c9c5d8713b3ea442f761e10c47d40
Tim <github@timgreen.ws>
2025-07-10 18:11:28 +1000

    changed positions of react projects

--- partials/work/work-all-react.php
+++ partials/work/work-all-react.php
@@ -127,6 +127,46 @@
 	</div>
 </article>
 
+
+<!-- react 11 -->
+<article id="react-11" class="project">
+	<div class="wrapper">
+		<header class="project-overview expandable">
+			<div>
+				<p class="overline">React</p>
+				<h3 class="large-heading">The Colour Contrast Checker</h3>
+				<div class="content">
+					<p>This is a React app that checks the colour contrast of your design based on the Web Content Accessibility Guidelines (WCAG). </p>
+					
+					<p>Features on this app are that it's accept HEX, RGB and HSL colour formats and converts them. If you like any colour contrasts you can save them as it can store those colours combinations in your local storage. 
+                    </p>
+				</div>
+				<dl class="details">
+					<dt>Visit</dt>
+					<dd><a href="http://colour.timgreen.website/" target="_blank">View Project</a></dd>
+					<dt>Git Repo</dt>
+					<dd><a href="https://github.com/tim-green/colour-contrast-checker" target="_blank">View Repo</a></dd>
+				</dl>
+			</div>
+		</header>
+		<div class="project-content">
+			<div class="gallery">
+				<div class="gallery-placeholder">
+					<img class="lazy-inline" data-src="../assets/images/react-slides/react-colour-contrast.webp" width="800" height="600" alt="Loading Image...">
+				</div>
+				<div class="gallery-wrapper">
+					<div class="gallery-item main dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-colour-contrast.webp" width="800" height="600" alt="to do list">
+					</div>
+					<div class="gallery-item dark">
+						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-colour-contrast2.webp" width="800" height="600" alt="mobile view of the todo list">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+
 <?php /*
 
 <!-- react 4 -->
@@ -470,44 +510,6 @@ Many of the onscreen ‘talent’ have many remomorable quotes, so I’ve built 
 </article>
 
 
-<!-- react 11 -->
-<article id="react-11" class="project">
-	<div class="wrapper">
-		<header class="project-overview expandable">
-			<div>
-				<p class="overline">React</p>
-				<h3 class="large-heading">The Colour Contrast Checker</h3>
-				<div class="content">
-					<p>This is a React app that checks the colour contrast of your design based on the Web Content Accessibility Guidelines (WCAG). </p>
-					
-					<p>Features on this app are that it's accept HEX, RGB and HSL colour formats and converts them. If you like any colour contrasts you can save them as it can store those colours combinations in your local storage. 
-                    </p>
-				</div>
-				<dl class="details">
-					<dt>Visit</dt>
-					<dd><a href="http://colour.timgreen.website/" target="_blank">View Project</a></dd>
-					<dt>Git Repo</dt>
-					<dd><a href="https://github.com/tim-green/colour-contrast-checker" target="_blank">View Repo</a></dd>
-				</dl>
-			</div>
-		</header>
-		<div class="project-content">
-			<div class="gallery">
-				<div class="gallery-placeholder">
-					<img class="lazy-inline" data-src="../assets/images/react-slides/react-colour-contrast.webp" width="800" height="600" alt="Loading Image...">
-				</div>
-				<div class="gallery-wrapper">
-					<div class="gallery-item main dark">
-						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-colour-contrast.webp" width="800" height="600" alt="to do list">
-					</div>
-					<div class="gallery-item dark">
-						<img class="gallery-lazy" data-src="../assets/images/react-slides/react-colour-contrast2.webp" width="800" height="600" alt="mobile view of the todo list">
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</article>
 
 <?php /* 
 <!-- react 12 -->
